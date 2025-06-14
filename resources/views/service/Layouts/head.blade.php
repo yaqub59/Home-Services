184283ccc28b1fc752c374b9641a61f2eec89d12
Muhammed Yaqub <yaqub@example.com>
2025-06-14 15:20:53 +0500

    commit

--- resources/views/service/Layouts/head.blade.php
+++ resources/views/service/Layouts/head.blade.php
@@ -10,20 +10,20 @@
     <link rel="shortcut icon" href="{{ asset('images/settings/' .Setting()->site_favicon) }}" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
     <!-- Fontawesome CSS -->
-    <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
 
     <!-- Feather CSS -->
-    <link rel="stylesheet" href="{{ url('assets/plugins/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/feather/feather.css') }}">
 
     <!-- Select2 CSS -->
-    <link rel="stylesheet" href="{{ url('assets/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
 
     <!-- Main CSS -->
-    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('admin-assets/plugins/izitoast/css/iziToast.min.css') }}">
 
 </head>
